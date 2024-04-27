5fb173058ce04728d1351eaffe460d9f201aaa51
rwanelmtary <153429078+rwanelmtary@users.noreply.github.com>
2024-04-27 18:09:05 +0300

    mvp
    refactor my part to mvp

--- SportSpot/favourite/FavouriteViewController.swift
+++ SportSpot/favourite/FavouriteViewController.swift
@@ -8,6 +8,33 @@
 import UIKit
 
 class FavouriteViewController: UIViewController {
+    
+    @IBOutlet weak var favTable: UITableView!
+    var activityIndicator: UIActivityIndicatorView!
+
+    
+    
+    func addNibFile() {
+        favTable.register(UINib(nibName: "FavouriteTableViewCell", bundle: nil), forCellReuseIdentifier: "favcell")
+//        self.favTable.dataSource = self
+//        self.favTable.delegate = self
+    }
+
+    func addLoadingIcon() {
+        activityIndicator = UIActivityIndicatorView(style: .large)
+        
+        activityIndicator.center = view.center
+        view.addSubview(activityIndicator)
+        activityIndicator.isHidden = false
+    }
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//       return 2
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//       ret 5
+//    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
