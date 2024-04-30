65563bbfe03e28e8ad22b70b670482144f510844
rwanelmtary <153429078+rwanelmtary@users.noreply.github.com>
2024-04-30 14:08:51 +0300

    creating unit testing files

---
+++ SportSpotTests/SportSpotTests.swift
@@ -0,0 +1,23 @@
+//
+//  SportSpotTests.swift
+//  SportSpotTests
+//
+//  Created by rwan elmtary on 30/04/2024.
+//
+
+import XCTest
+@testable import SportSpot
+
+final class SportSpotTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+   
+
+}
